7571e2a4db56ccee7e5d5c155762bd4f81dcfdee
Claire <clairelogan16@gmail.com>
2021-11-11 14:53:02 -0800

    added timing toggle on pie chart

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,22 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :first_name, :avatar_url, :watched_entries_by_streamer
+  attributes :id, :username, :email, :first_name, :avatar_url, :watched_entries_by_streamer, :watched_entries_by_streamer_365D, :watched_entries_by_streamer_90D
 
   def watched_entries_by_streamer
     statusid = Status.find_by(name: "Watched")
     self.object.entries.where(status_id: statusid.id).group(:streamer_id).count
+end
+
+# within last 365 days
+def watched_entries_by_streamer_365D
+  statusid = Status.find_by(name: "Watched")
+  self.object.entries.where(status_id: statusid.id, created_at: 1.year.ago..).group(:streamer_id).count
+end
+
+
+# within last 90 days
+def watched_entries_by_streamer_90D
+  statusid = Status.find_by(name: "Watched")
+  self.object.entries.where(status_id: statusid.id, created_at: 3.months.ago..).group(:streamer_id).count
 end
 
 end
--- client/src/Components/MyDashboard/Charts.js
+++ client/src/Components/MyDashboard/Charts.js
@@ -1,26 +1,59 @@
 import { useSelector } from "react-redux";
+import { useState } from 'react'
 import { PieChart } from 'react-chartkick'
 // import { PieChart, Pie } from 'recharts';
 import 'chartkick/chart.js'
+import { 
+    Typography,
+    Box
+ } from '@mui/material';
 
 
 function Charts() {
     const user = useSelector((state) => state.user.user);
     const streamers = useSelector((state) => state.entries.streamers);
-    const { watched_entries_by_streamer } = user
+    const { watched_entries_by_streamer, watched_entries_by_streamer_365D, watched_entries_by_streamer_90D } = user
 
+    const [lookback, setLookback] = useState('all')
+
+
+    let data;
+    if(lookback === "year"){
+        data = watched_entries_by_streamer_365D
+    } else if (lookback === "all"){
+        data = watched_entries_by_streamer
+    } else if (lookback === "3month"){
+        data = watched_entries_by_streamer_90D 
+    }
+
+    // console.log("from dash", data)
 
     let chartData = {}
     streamers.forEach(streamer => {
-        if(watched_entries_by_streamer[streamer.id] > 0){
-            chartData[streamer.name] = watched_entries_by_streamer[streamer.id]
+        if(data[streamer.id] > 0){
+            chartData[streamer.name] = data[streamer.id]
         }
     })
+
+    function handleTimeChange(e){
+        setLookback(e.target.value)
+    }
 
   //console.log(chartData)
 
     return (
-        <PieChart data={chartData} donut={true} />
+        <Box>
+            <Box>
+                <select onChange={handleTimeChange}>
+                    <option value="all">all time</option>
+                    <option value="year">past year</option>
+                    <option value="3month">past 3 months</option>
+                </select>
+            </Box>
+            <Box>
+                <PieChart data={chartData} donut={true} />
+            </Box>
+        </Box>
     )
 }
 
